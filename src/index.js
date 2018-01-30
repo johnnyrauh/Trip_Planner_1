103fa6ace36b4dcddacece7646a10bab66e26e91
Aaron Young <brainomite@gmail.com>
2018-01-30 12:59:49 -0500

    marker factory completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 "use strict";
 
-console.log("bar foo");
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1IjoiYnJhaW5vbWl0ZSIsImEiOiJjamQxd20wMm8xa25iMnFvNGh5cWZjcXFyIn0.EmncSI52lbTJJad3a2Ok8g";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available
+});
+
+module.exports = map
+
+const { createActivityMarker, createRestaurantMarker, createHotelMarker } = require('./marker')
+
+
+createHotelMarker(map,[-74.009, 40.705]);
